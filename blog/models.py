24df9d1e20fc700f34274b701b0518981ebecfc4
Lei Wang <wanglei.okay@gmail.com>
2020-07-25 20:16:00 +0300

    feat: add blog page

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from wagtail.api import APIField
 from wagtail.core.models import Orderable, Page
 from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class BlogPageAuthor(Orderable):
     page = models.ForeignKey('blog.BlogPage', on_delete=models.CASCADE, related_name='authors')
@@ -14,15 +16,19 @@ class BlogPageAuthor(Orderable):
 
 
 class BlogPage(Page):
-    published_date = models.DateTimeField()
-    body = RichTextField()
+    body = RichTextField(blank=True)
     feed_image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE)
     private_field = models.CharField(max_length=255)
 
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+        ImageChooserPanel("feed_image"),
+    ]
+
     # Export fields over the API
     api_fields = [
-        APIField('published_date'),
         APIField('body'),
         APIField('feed_image'),
-        APIField('authors'),  # This will nest the relevant BlogPageAuthor objects in the API response
+        #APIField('authors'),  # This will nest the relevant BlogPageAuthor objects in the API response
     ]
